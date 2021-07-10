a8bc70d582f98e4741c29f0a7a687afec88c6879
Terry_Kim <48509331+jihs2113@users.noreply.github.com>
2021-07-11 03:07:59 +0900

    poset and popular setting

--- components/Movies/Slide.js
+++ components/Movies/Slide.js
@@ -4,6 +4,7 @@ import PropTypes from "prop-types";
 import { apiImage } from "../../api";
 import Poster from "../Poster";
 import { TouchableOpacity } from "react-native";
+import Votes from "../Votes";
 
 const Container = styled.View`
   height: 100%;
@@ -15,30 +16,25 @@ const BG = styled.Image`
   opacity: 0.4;
   position: absolute;
 `;
-
 const Content = styled.View`
   height: 100%;
   flex-direction: row;
   align-items: center;
   justify-content: space-around;
 `;
-
 const Data = styled.View`
   width: 50%;
   align-items: flex-start;
 `;
-
 const Title = styled.Text`
   color: white;
   font-weight: bold;
   font-size: 19px;
   margin-bottom: 10px;
 `;
-
-const Votes = styled.Text`
-  color: rgb(220, 220, 220);
+const VotesContainer = styled.Text`
   margin-bottom: 7px;
-  font-size: 12px;
+
 `;
 
 const Overview = styled.Text`
@@ -46,27 +42,27 @@ const Overview = styled.Text`
   font-size: 14px;
   font-weight: 500;
 `;
-
 const Button = styled.View`
   margin-top: 10px;
   background-color: #e74c3c;
-  padding: 10px;
+  padding: 7px 10px;
   border-radius: 3px;
 `;
 
 const ButtonText = styled.Text`
   color: white;
 `;
-
 const Slide = ({ id, title, backgroundImage, votes, overview, poster }) => (
   <Container>
     <BG source={{ uri: apiImage(backgroundImage) }} />
     <Content>
       <Poster url={apiImage(poster)} />
       <Data>
-        <Title>{title.slice(0, 30)}</Title>
-        <Votes>⭐️ {votes} / 10</Votes>
-        <Overview>{overview.slice(0, 120)}</Overview>
+        <Title>{title.length > 40 ? `${title.slice(0, 40)}...` : title}</Title>
+        <VotesContainer>
+          <Votes votes={votes}/>
+        </VotesContainer>
+        <Overview>{overview.slice(0, 110)}...</Overview>
         <TouchableOpacity>
           <Button>
             <ButtonText>View details</ButtonText>
--- screens/Movies/MoviesPresenter.js
+++ screens/Movies/MoviesPresenter.js
@@ -1,47 +1,70 @@
 import React from "react";
 import styled from "styled-components/native"
 import Swiper from "react-native-web-swiper";
-import { Dimensions, ActivityIndicator, Slider, View  } from "react-native";
+import { Dimensions, ActivityIndicator, ScrollView, Slider, View  } from "react-native";
 import Slide from "../../components/Movies/slide";
+import Title from "../../components/Title";
+import Vertical from "../../components/Vertical";
 
-const { width: WIDTH, height: HEIGHT } = Dimensions.get("screen");
+
+const { width: WIDTH, height: HEIGHT } = Dimensions.get("window");
 
 const Container = styled.View`
-  flex: 1;
-  background-color: black;
-  justify-content: center;
+
 `;
 
 
 const SliderContainer = styled.View`
   width: ${WIDTH}px;
   height: ${HEIGHT / 4}px;
+  margin-bottom: 40px;
 `;
 
 
-export default ({loading, nowPlaying}) => {
-  <Container>
+export default ({ loading, nowPlaying, popular }) => (
+  <ScrollView
+        style={{
+          backgroundColor: "black"
+        }}
+        contentContainerStyle={{
+          flex: 1,
+          justifyContent: loading ? "center" : "flex-start"
+        }}
+      >
     {loading ? (
-      <ActivityIndicator color="white" size="small"/>
-      ) : (
-      <SliderContainer>
-        <Swiper controlsEnabled={false} loop timeout={3}>
-          {nowPlaying.map(movie => (
-            // <Section key={movie.id }>
-            //   <Text>{movie.original_title}</Text>
-            // </Section>
-            <Slide
-            key={movie.id}
-            id={movie.id}
-            title={movie.original_title}
-            overview={movie.overview}
-            votes={movie.vote_average}
-            backgroundImage={movie.backdrop_path}
-            poster={movie.poster_path}
-            />
-          ))}
-        </Swiper>
-      </SliderContainer>
+      <ActivityIndicator color="white" size="small" />
+    ) : (
+      
+      <>
+        <SliderContainer>
+          <Swiper controlsEnabled={false} loop timeout={3}>
+            {nowPlaying.map(movie => (
+              <Slide
+                key={movie.id}
+                id={movie.id}
+                title={movie.original_title}
+                overview={movie.overview}
+                votes={movie.vote_average}
+                backgroundImage={movie.backdrop_path}
+                poster={movie.poster_path}
+              />
+            ))}
+          </Swiper>
+        </SliderContainer>
+        <Container>
+          <Title title={"Popular Movies"} />
+          <ScrollView horizontal>
+              {popular.map(movie => (
+                <Vertical
+                  key={movie.id}
+                  poster={movie.poster_path}
+                  title={movie.original_title}
+                  votes={movie.vote_average}
+                />
+              ))}
+          </ScrollView>
+        </Container>
+      </>
     )}
-  </Container>
-};
+  </ScrollView>
+);
