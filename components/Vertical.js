0c3b587de79f887056b7003126efb8fe714e22d4
Terry_Kim <48509331+jihs2113@users.noreply.github.com>
2021-07-23 17:42:47 +0900

    search styled setting

--- components/Vertical.js
+++ components/Vertical.js
@@ -1,17 +1,33 @@
 import React from "react";
-import Title from "./Title";
-import { ScrollView } from "react-native";
-
-export default ({ title, children }) => (
-  <>
-    <Title title={title} />
-    <ScrollView
-      style={{ marginTop: 20, marginBottom: 40 }}
-      contentContainerStyle={{ paddingLeft: 30 }}
-      horizontal
-      showsHorizontalScrollIndicator={false}
-    >
-      {children}
-    </ScrollView>
-  </>
+import styled from "styled-components/native";
+import PropTypes from "prop-types";
+import { TouchableOpacity } from "react-native";
+import { trimText } from "../utils";
+import Poster from "./Poster";
+import Votes from "./Votes";
+const Container = styled.View`
+  align-items: center;
+  margin-right: 20px;
+`;
+const Title = styled.Text`
+  color: white;
+  font-weight: 500;
+  margin: 10px 0px 5px 0px;
+`;
+const Vertical = ({ id, poster, title, votes }) => (
+  <TouchableOpacity>
+    <Container>
+      <Poster url={poster} />
+      <Title>{trimText(title, 10)}</Title>
+      {votes > 0 && <Votes votes={votes} />}
+    </Container>
+  </TouchableOpacity>
 );
+
+Vertical.propTypes = {
+  id: PropTypes.number.isRequired,
+  poster: PropTypes.string,
+  title: PropTypes.string.isRequired,
+  votes: PropTypes.number.isRequired
+};
+export default Vertical;
--- utils.js
+++ utils.js
@@ -1,4 +1,4 @@
-export const trimText = (text, limit) =>
+export const trimText = (text = "", limit) =>
   text.length > limit ? `${text.slice(0, limit)}...` : text;
 
 export const formatDate = date => {
@@ -8,6 +8,6 @@ export const formatDate = date => {
     month: "long",
     year: "numeric"
   });
+};
   //날짜를 표시해줄건데 toLocaleDateString()을 사용
   // July, 16, 2021 이런식으로 컴파일된다. 또는 2021년 7월 16일
-};
