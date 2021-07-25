89946955f760d70d4af9426e89f1d4d6bb48542b
Terry_Kim <48509331+jihs2113@users.noreply.github.com>
2021-07-25 20:42:21 +0900

    Detail setting

--- components/Horizontal.js
+++ components/Horizontal.js
@@ -6,7 +6,7 @@ import Poster from "./Poster";
 import Votes from "./Votes";
 import { apiImage } from "../api";
 import { trimText, formatDate } from "../utils";
-
+import { useNavigation } from "@react-navigation/native";
 
 const Container = styled.View`
   padding: 0px 30px;
@@ -14,13 +14,11 @@ const Container = styled.View`
   flex-direction: row;
   align-items: flex-start;
 `;
-
 const Data = styled.View`
   align-items: flex-start;
   width: 65%;
   margin-left: 25px;
 `;
-
 const Title = styled.Text`
   color: white;
   font-weight: bold;
@@ -31,28 +29,39 @@ const ReleaseDate = styled.Text`
   opacity: 0.8;
   font-size: 12px;
 `;
-
 const Overview = styled.Text`
   margin-top: 10px;
-  opacity: 0.8;
   color: white;
+  opacity: 0.8;
 `;
 
 
-const Horizontal = ({ id, title, poster, overview, releaseDate }) => (
-  <TouchableOpacity>
-    <Container>
-      <Poster url={poster} />
-      <Data>
-        <Title>{trimText(title, 30)}</Title>
-        {releaseDate ? (
-          <ReleaseDate>{formatDate(releaseDate)}</ReleaseDate>
+const Horizontal = ({ id, title, poster, overview, releaseDate }) => {
+  const navigation = useNavigation();
+  const goToDetai = () => {
+    navigation.navigate("Detail", {
+      id,
+      title,
+      poster,
+      overview,
+      releaseDate
+    });
+  };
+  return (
+    <TouchableOpacity onPress={goToDetai}>
+      <Container>
+        <Poster url={poster} />
+        <Data>
+          <Title>{trimText(title, 30)}</Title>
+          {releaseDate ? (
+            <ReleaseDate>{formatDate(releaseDate)}</ReleaseDate>
           ) : null}
-        <Overview>{trimText(overview, 80)}</Overview>
-      </Data>
-    </Container>
-  </TouchableOpacity>
-);
+          <Overview>{trimText(overview, 80)}</Overview>
+        </Data>
+      </Container>
+    </TouchableOpacity>
+  );
+};
 
 Horizontal.propTypes = {
   id: PropTypes.number.isRequired,
