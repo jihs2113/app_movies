75892b488dcf02c9d94c5a851cfc5d9a40107aff
Terry_Kim <48509331+jihs2113@users.noreply.github.com>
2021-07-27 18:27:00 +0900

    add a detail info

--- screens/Detail/DetailPresenter.js
+++ screens/Detail/DetailPresenter.js
@@ -6,51 +6,43 @@ import { Dimensions, ActivityIndicator } from "react-native";
 import Poster from "../../components/Poster";
 import Votes from "../../components/Votes";
 import { formatDate } from "../../utils";
-
 const BG = styled.Image`
   width: 100%;
   height: 100%;
   opacity: 0.4;
   position: absolute;
 `;
-
 const Header = styled.View`
   height: ${Dimensions.get("window").height / 3}px;
   align-items: center;
   justify-content: flex-end;
 `;
-
 const Container = styled.View`
   flex-direction: row;
   align-items: center;
   top: 30px;
 `;
-
 const Info = styled.View`
   width: 50%;
   margin-left: 40px;
 `;
-
 const Title = styled.Text`
   color: white;
   font-weight: 600;
   font-size: 24px;
   margin-bottom: 10px;
 `;
-
 const Data = styled.View`
   margin-top: 30px;
   padding: 0px 30px;
 `;
-
 const DataName = styled.Text`
-margin-top: 30px;
+  margin-top: 30px;
   color: white;
   opacity: 0.8;
   font-weight: 800;
   margin-bottom: 15px;
 `;
-
 const DataValue = styled.Text`
   color: white;
   opacity: 0.8;
@@ -58,7 +50,10 @@ const DataValue = styled.Text`
 `;
 
 export default ({ result, loading }) => (
-  <ScrollContainer loading={false}>
+  <ScrollContainer
+    loading={false}
+    contentContainerStyle={{ paddingBottom: 80 }}
+  >
     <>
       <Header>
         <BG source={{ uri: apiImage(result.backgroundImage, "-") }} />
@@ -92,6 +87,42 @@ export default ({ result, loading }) => (
           <>
             <DataName>Release Date</DataName>
             <DataValue>{formatDate(result.release_date)}</DataValue>
+          </>
+        )}
+        {result.status && (
+          <>
+            <DataName>Status</DataName>
+            <DataValue>{result.status}</DataValue>
+          </>
+        )}
+        {result.runtime && (
+          <>
+            <DataName>Runtime</DataName>
+            <DataValue>{result.runtime} minutes</DataValue>
+          </>
+        )}
+        {result.first_air_date && (
+          <>
+            <DataName>First Air Date</DataName>
+            <DataValue>{formatDate(result.first_air_date)}</DataValue>
+          </>
+        )}
+        {result.genres && (
+          <>
+            <DataName>Genres</DataName>
+            <DataValue>
+              {result.genres.map((g, index) =>
+                index + 1 === result.genres.length ? g.name : `${g.name}, `
+              )}
+            </DataValue>
+          </>
+        )}
+        {result.number_of_episodes && (
+          <>
+            <DataName>Seasons / Episodes</DataName>
+            <DataValue>
+              {result.number_of_seasons} / {result.number_of_episodes}
+            </DataValue>
           </>
         )}
       </Data>
